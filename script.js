d83271d0540b3eb332910399e7da0e9d71f4569d
Isoqova avoqosI <isoqovaogilshod@gmail.com>
2023-11-20 15:17:55 +0000

    conatiner

--- script.js
+++ script.js
@@ -8,3 +8,29 @@ menu.addEventListener("click", () => {
 linksPart.addEventListener("click", () => {
   linksPart.classList.toggle("hide");
 });
+
+/////////////////////////////
+const url = "https://url-shortener-service.p.rapidapi.com/shorten";
+const options = {
+  method: "POST",
+  headers: {
+    "content-type": "application/x-www-form-urlencoded",
+    "X-RapidAPI-Key": "a486227ea2mshe421b0e17360a4fp10cc5djsn73d41f8a3c59",
+    "X-RapidAPI-Host": "url-shortener-service.p.rapidapi.com",
+  },
+  body: new URLSearchParams({
+    url: "https://google.com/",
+  }),
+};
+
+async function foo() {
+  try {
+    const response = await fetch(url, options);
+    const result = await response.text();
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+foo();
