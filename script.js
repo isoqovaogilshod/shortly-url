86e7b015f0424fa8f1683e50a622f02c3ca2c673
Isoqova avoqosI <isoqovaogilshod@gmail.com>
2023-11-21 15:18:10 +0000

    script

--- script.js
+++ script.js
@@ -9,28 +9,46 @@ linksPart.addEventListener("click", () => {
   linksPart.classList.toggle("hide");
 });
 
+//////////////////////////////
+let input = document.getElementById("input");
+let shorentBtn = document.getElementById("shortenBtn");
+let copyBtn = document.getElementById("copyBtn");
+let link = document.getElementById("link");
+let shortLink = document.getElementById("shortLink");
+let shortUrls = document.getElementById("shortUrls");
 /////////////////////////////
-const url = "https://url-shortener-service.p.rapidapi.com/shorten";
-const options = {
-  method: "POST",
-  headers: {
-    "content-type": "application/x-www-form-urlencoded",
-    "X-RapidAPI-Key": "a486227ea2mshe421b0e17360a4fp10cc5djsn73d41f8a3c59",
-    "X-RapidAPI-Host": "url-shortener-service.p.rapidapi.com",
-  },
-  body: new URLSearchParams({
-    url: "https://google.com/",
-  }),
-};
 
-async function foo() {
+async function copyContent(text) {
+  try {
+    await navigator.clipboard.writeText(text);
+    console.log("Content copied to clipboard");
+  } catch (err) {
+    console.error("Failed to copy: ", err);
+  }
+}
+/////////////////////////////
+
+async function shortenIt() {
+  const url = "https://url-shortener-service.p.rapidapi.com/shorten";
+  const options = {
+    method: "POST",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      "X-RapidAPI-Key": "a486227ea2mshe421b0e17360a4fp10cc5djsn73d41f8a3c59",
+      "X-RapidAPI-Host": "url-shortener-service.p.rapidapi.com",
+    },
+    body: new URLSearchParams({
+      url: "https://www.youtube.com/",
+    }),
+  };
   try {
     const response = await fetch(url, options);
-    const result = await response.text();
+    // console.log(response.status);
+    const result = await response.json();
     console.log(result);
   } catch (error) {
     console.error(error);
   }
 }
 
-foo();
+shortenIt();
